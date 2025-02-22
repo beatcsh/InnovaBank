bdb9627e928bd1b260c1ee830dc072289f519207
zaimont <montserratlozano03@gmail.com>
2025-02-22 01:35:33 -0600

    se agrego ruta para el controlador de transactions

---
+++ server/backend/routes/transactionsRoutes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import transactionsController from "../controllers/transactionsController.js";
+
+const router = Router();
+
+router.post("/create", transactionsController.create); // Crear una transacción
+router.get("/all", transactionsController.getAll); // Obtener todas las transacciones
+
+export default router;
