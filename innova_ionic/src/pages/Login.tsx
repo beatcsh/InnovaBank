728bcf25f40c9813fac958922cd5ecb29b2b1f42
Jesus Sanchez <josusssanher@gmail.com>
2025-03-13 00:57:26 -0600

    olvide algo en la estandarizacion, ahi va pal CI/CD

--- innova_ionic/src/pages/Login.tsx
+++ innova_ionic/src/pages/Login.tsx
@@ -1,4 +1,34 @@
+import { useState } from "react";
+import { useHistory } from "react-router";
+import axios from "axios";
+
 const Login: React.FC = () => {
+
+    const [data, setData] = useState({});
+    const history = useHistory();
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        e.preventDefault();
+        setData({ ...data, [e.target.name]: e.target.value });
+    }
+
+    const onSubmit = async () => {
+        try {
+    
+            const response = await axios.post("https://innovabank.onrender.com/users/login", data);
+            
+            alert("inicio de sesion exitoso")
+            
+            const authToken = response.data.token;
+            localStorage.setItem("authToken", authToken);
+    
+            history.push("/home", { token: authToken });
+        } catch (err: any) {
+            alert("tuvimos problemas")
+        }
+    };
+    
+
     return (
         <>
             <div className='w-[100%] h-[100vh] flex flex-col place-items-center space-y-2 bg-violet-700'>
@@ -9,9 +39,9 @@ const Login: React.FC = () => {
                     <img src="../assets/logo.jpeg" alt="innovalogo" className='w-[190px] rounded-full' />
                 </div>
                 <form className='w-[100%] grid grid-cols-1 place-items-center my-6 gap-5'>
-                    <input type="email" placeholder='Email' className='w-[80%] h-[50px] bg-white rounded-2xl text-black placeholder-gray-700 px-4' />
-                    <input type="Password" placeholder='Password' className='w-[80%] h-[50px] bg-white rounded-2xl text-black placeholder-gray-700 px-4' />
-                    <button type="submit" className="bg-violet-900 font-semibold w-[80%] h-[50px] !rounded-2xl text-white hover:bg-violet-600 transition-all duration-500">
+                    <input onChange={onChange} name="email" type="email" placeholder='Email' className='w-[80%] h-[50px] bg-white rounded-2xl text-black placeholder-gray-700 px-4' />
+                    <input onChange={onChange} name="contrasena" type="password" placeholder='Password' className='w-[80%] h-[50px] bg-white rounded-2xl !text-black placeholder-gray-700 px-4' />
+                    <button onClick={onSubmit} type="button" className="bg-violet-900 font-semibold w-[80%] h-[50px] !rounded-2xl text-white hover:bg-violet-600 transition-all duration-500">
                         Iniciar Sesion
                     </button>
                     <a href='/register' className='w-[80%]'>
